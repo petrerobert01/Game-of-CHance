b7318d28e9a34fb2b45da517be763e868d507335
petrerobert01 <r.petregrigorie@gmail.com>
2020-07-08 13:34:36 +0300

    Play some games

--- games.py
+++ games.py
@@ -6,33 +6,40 @@ int( x )
 y = all_pot - x
 money = all_pot - y
 
+print("Games you can play. \n 0.Coin Flip \n 1.Dice Roll \n 2.High/Low card game \n 3. Roullete")
+
 #Write your game of chance functions herec
 choice = input('Your game: ')
 number_of_games = input('How many games you want to play?\n')
 
+
 for game in range(0,4):
+    money_left = all_pot
     for number in range(0, number_of_games):
         if game == choice and game == 0:
-            def coin_flip(bet, guess, all_pot):
+            def coin_flip(bet, guess):
                 if (bet > money):
                     return "You don't have enought money"
                 num = random.randint(1, 2)
 
                 if (num == 1 and guess == "Heads"):
-                    all_pot += bet * 2
-                    return ("You Win:{} \n Your money:{}".format(bet * 2,all_pot))
+                    return bet * 2
                 elif (num == 2 and guess == "Tails"):
-                    all_pot += bet * 2
-                    return ("You Win:{} \n Your money:{}".format(bet * 2,all_pot))
+                    return bet * 2
                 else:
-                    all_pot = all_pot - bet
-                    return ("You lost:{} \n YOur money:{}".format(-bet, all_pot))
+                    return -bet
             guess = "Heads"
-            print(coin_flip(money, guess, all_pot))
+            how_mutch = coin_flip(money, guess)
+            money_left += how_mutch
+            if money < how_mutch:
+                money_left = money_left - money
+                print("You Win:{} \nYour money:{}".format(how_mutch - money, money_left))
+            elif money > how_mutch:
+                print("You Lose:{} \nYour money:{}".format(how_mutch, money_left))
             print("\n")
-        #print(money)
+
         elif game == choice and game == 1:
-                def cho_han(guess, bet, all_pot):
+                def cho_han(guess, bet):
                     if (bet > money):
                         return "You don't have enought money"
                     dice1 = random.randint(1, 6)
@@ -41,21 +48,24 @@ for game in range(0,4):
                     print("The dice roll is " + str(sum))
 
                     if (sum % 2 == 0 and guess == "Even"):
-                        all_pot += bet * 2
-                        return ("You Win:{} \n Your money:{}".format(bet * 2,all_pot))
+                        return bet * 2
                     elif (sum % 2 != 0 and guess == "Odd"):
-                        all_pot += bet * 2
-                        return ("You Win:{} \n Your money:{}".format(bet * 2,all_pot))
+                        return bet * 2
                     else:
-                        all_pot = all_pot - bet
-                        return ("You lost:{} \n YOur money:{}".format(-bet, all_pot))
-                print(cho_han("Even", money, all_pot))
+                        return -bet
+                guess = "Even"
+                how_mutch = cho_han(guess, money)
+                money_left += how_mutch
+                if money < how_mutch:
+                    money_left = money_left - money
+                    print("You Win:{} \nYour money:{}".format(how_mutch - money, money_left))
+                elif money > how_mutch:
+                    print("You Lose:{} \nYour money:{}".format(how_mutch, money_left))
                 print("\n")
-        #print(money)
 
 
         elif game == choice and game == 2:
-            def high_low(bet, all_pot):
+            def high_low(bet):
                 if (bet > money):
                     return "You don't have enought money"
 
@@ -71,21 +81,27 @@ for game in range(0,4):
                 card_deck.remove(card2)
 
                 if card1[0] > card2[0]:
-                    all_pot += bet * 2
-                    return ("You Win:{} \n Your money:{}".format(bet * 2,all_pot))
+                    return bet * 2
                 elif card1[0] == card2[0]:
-                    return "It's a tie"
+                    return bet
                 else:
-                    all_pot = all_pot - bet
-                    return ("You lost:{} \n YOur money:{}".format(-bet, all_pot))
+                    return -bet
 
-            print(high_low(money, all_pot))
+            how_mutch = high_low(money)
+            money_left += how_mutch
+            if money < how_mutch:
+                money_left = money_left - money
+                print("You Win:{} \nYour money:{}".format(how_mutch - money, money_left))
+            elif money > how_mutch:
+                print("You Lose:{} \nYour money:{}".format(how_mutch, money_left))
+            elif money == how_mutch:
+                print("It's a tie")
             print("\n")
         #print(money)
 
 
         elif game == choice and game == 3:
-            def roullete(bet, guess, all_pot):
+            def roullete(bet, guess):
                 if (bet > money):
                     return "You don't have enought money"
 
@@ -101,24 +117,26 @@ for game in range(0,4):
 
 
                 if guess == "Even" and number_on_roullete[0] % 2 == 0:
-                    all_pot += bet * 2
-                    return ("You Win:{} \n Your money:{}".format(bet * 2,all_pot))
+                    return bet * 2
                 elif guess == "Odd" and number_on_roullete[0] % 2 != 0:
-                    all_pot += bet * 2
-                    return ("You Win:{}".format(bet * 2))
+                    return bet * 2
                 elif guess == 0 and number_on_roullete[0] == 0:
-                    all_pot += bet * 2
-                    return ("You Win:{}".format(bet * 35))
+                    return bet * 35
                 elif guess == number_on_roullete[0]:
-                    all_pot += bet * 2
-                    return ("You Win:{}".format(bet * 35))
+                    return bet * 35
                 else:
-                    all_pot = all_pot - bet
-                    return ("You lost:{}".format(-bet))
+                    return -bet
 
 
-
-            print(roullete(money, 15,all_pot))
+            how_mutch = roullete(money, 15)
+            money_left += how_mutch
+            if money < how_mutch:
+                money_left = money_left - money
+                print("You Win:{} \nYour money:{}".format(how_mutch - money, money_left))
+            elif money > how_mutch:
+                print("You Lose:{} \nYour money:{}".format(how_mutch, money_left))
+            elif money == how_mutch:
+                print("It's a tie")
             print("\n")
         #print(money)
 
